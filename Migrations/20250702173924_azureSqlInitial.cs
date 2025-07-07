b335dd23dba0e1aae79562f0b8afe8654490750c
Iza <mrs.master.of.disaster@gmail.com>
2025-07-07 09:44:44 +0200

    Zmieniono auth na usera z readonly

---
+++ Migrations/20250702173924_azureSqlInitial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KatalogPrototyp.Migrations
+{
+    /// <inheritdoc />
+    public partial class azureSqlInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
